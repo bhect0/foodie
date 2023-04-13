5b7c38791a13b1a0926434fe1e49ceb676d439fb
Héctor B <hector.benedicte@gmail.com>
2023-04-13 12:11:04 +0200

    Creación proyecto Android. Estructura pantalla de Login.

---
+++ app/src/main/java/tfg/hector/foodie/MainActivity.java
@@ -0,0 +1,15 @@
+package tfg.hector.foodie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+}
